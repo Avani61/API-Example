c8d14771963f23358a6b71b9ff40e4185704d464
Avani61 <110109098+Avani61@users.noreply.github.com>
2023-02-28 15:13:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostForm from './components/PostForm'
+function App() {
+  return (
+    <div className="App">
+      <PostForm />
+    </div>
+  );
+}
+
+export default App;
